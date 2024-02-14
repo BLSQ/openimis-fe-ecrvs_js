dddc822863391bba22d7269bccb01528c3b9fa92
Thibault Dethier <tdethier@bluesquarehub.com>
2024-02-14 16:34:49 +0100

    Removed MainMenu if no rights
    The MainMenu was still appearing when a logged in user didn't have any eCRVS rights.

--- src/menu/EcrvsMainMenu.js
+++ src/menu/EcrvsMainMenu.js
@@ -3,28 +3,40 @@ import { injectIntl } from 'react-intl';
 import { connect } from 'react-redux';
 import { NotificationsActive, Subscriptions } from '@material-ui/icons';
 import { formatMessage, MainMenuContribution, withModulesManager } from '@openimis/fe-core';
-import { ECRVS_MAIN_MENU_CONTRIBUTION_KEY } from '../constants';
+import {
+  ECRVS_MAIN_MENU_CONTRIBUTION_KEY,
+  RIGHT_HERA_NOTIFICATIONS_SEARCH,
+  RIGHT_HERA_SUBSCRIPTIONS_SEARCH
+} from '../constants';
 
 function EcrvsMainMenu(props) {
+  let entries = [];
 
-  const entries = [
-    {
-      text: formatMessage(props.intl, 'ecrvs', 'menu.heraSubscriptions'),
-      icon: <Subscriptions />,
-      route: '/ecrvs/heraSubscriptions',
-    },
-    {
-      text: formatMessage(props.intl, 'ecrvs', 'menu.heraNotifications'),
-      icon: <NotificationsActive />,
-      route: '/ecrvs/heraNotifications',
-    },
-  ];
+  if (props.rights.includes(RIGHT_HERA_SUBSCRIPTIONS_SEARCH)) {
+    entries.push(
+      {
+        text: formatMessage(props.intl, 'ecrvs', 'menu.heraSubscriptions'),
+        icon: <Subscriptions />,
+        route: '/ecrvs/heraSubscriptions',
+      }
+    );
+  }
+  if (props.rights.includes(RIGHT_HERA_NOTIFICATIONS_SEARCH)) {
+    entries.push(
+      {
+        text: formatMessage(props.intl, 'ecrvs', 'menu.heraNotifications'),
+        icon: <NotificationsActive />,
+        route: '/ecrvs/heraNotifications',
+      }
+    );
+  }
 
   entries.push(
     ...props.modulesManager
       .getContribs(ECRVS_MAIN_MENU_CONTRIBUTION_KEY)
       .filter((c) => !c.filter || c.filter(props.rights)),
   );
+  if (!entries.length) return null;
 
   return (
     <MainMenuContribution
